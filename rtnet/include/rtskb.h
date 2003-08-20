10f76cb6c98863d89b333fd6d238ea5fb176a4a3
Jan Kiszka <jan.kiszka@web.de>
2003-08-20 16:18:35 +0000

    * implemented new pool mechanisms * added priority queue functions * source code cleanup
    git-svn-id: https://rtnet.svn.sourceforge.net/svnroot/rtnet/trunk@158 be94bdbd-1fe7-0310-aeeb-c95d3c307607

--- rtnet/include/rtskb.h
+++ rtnet/include/rtskb.h
@@ -23,7 +23,7 @@
 
 #ifdef __KERNEL__
 
-#include <rtai.h>
+#include <rtnet_internal.h>
 
 
 struct rtskb_head;
@@ -37,6 +37,9 @@ struct rtskb {
     struct rtskb        *next;
 
     struct rtskb_head   *head;
+    struct rtskb_head   *pool;
+
+    unsigned int        priority;
 
     struct rtsocket     *sk;
     struct rtnet_device *rtdev;
@@ -69,9 +72,6 @@ struct rtskb {
     unsigned short      protocol;
     unsigned char       pkt_type;
 
-    unsigned int        users;
-    unsigned char       cloned;
-
     unsigned int        csum;
     unsigned char       ip_summed;
 
@@ -98,6 +98,12 @@ struct rtskb_head {
     spinlock_t          lock;
 };
 
+struct rtskb_prio_list {
+    spinlock_t          lock;
+    __u32               usage;
+    struct rtskb_head   queue[0];
+};
+
 
 #if defined(CONFIG_RTNET_MM_VMALLOC)
     #define rt_mem_alloc(size)  malloc (size)
@@ -107,52 +113,203 @@ struct rtskb_head {
     #define rt_mem_free(addr)   kfree (addr)
 #endif
 
+#define DATA_BUF_ALIGN          16 /* align data on 16 bytes boundaries */
 
-/****
- * The number of free rtskb's in pool is handled by two srq's. inc_pool_srq means
- * creating rtskbs and dec_pool_srq is the opposite of it.
- */
+/* The rtskb structure and its data buffer are allocated as one chunk. This
+   macro aligns the beginning of the data buffer according to DATA_BUF_ALING
+   by adjusting the structure size. */
+#define ALIGN_RTSKB_LEN         \
+    ((sizeof(struct rtskb)+DATA_BUF_ALIGN-1) & ~(DATA_BUF_ALIGN-1))
 
 /* default values for the module parameter */
-#define DEFAULT_RTSKB_POOL_DEF  24  /* default number of rtskb's in pool */
-#define DEFAULT_MIN_RTSKB_DEF   16  /* create new rtskb if less then DEFAULT_MIN_RTSKB in Pool */
-#define DEFAULT_MAX_RTSKB_DEF   32  /* delete rtskb if more then DEFAULT_MAX_RTSKB in Pool */
+#define DEFAULT_GLOBAL_RTSKBS   0  /* default number of rtskb's in global pool */
+#define DEFAULT_DEVICE_RTSKBS   16  /* default additional rtskbs per network adapter */
+#define DEFAULT_SOCKET_RTSKBS   16  /* default number of rtskb's in socket pools */
 #define DEFAULT_MAX_RTSKB_SIZE  ETH_FRAME_LEN + 2 + 20
                                     /* 2  = offset to align IP header */
                                     /* 20 = max. additional space needed by eepro100-rt */
 
-extern unsigned int rtskb_pool_pool;    /* needed for /proc/... in rtnet_module.c */
-extern unsigned int rtskb_pool_min;
-extern unsigned int rtskb_pool_max;
+extern unsigned int socket_rtskbs;      /* default number of rtskb's in socket pools */
+extern unsigned int rtskb_max_size;     /* rtskb data buffer size */
 
+extern unsigned int rtskb_pools;        /* current number of rtskb pools      */
+extern unsigned int rtskb_pools_max;    /* maximum number of rtskb pools      */
 extern unsigned int rtskb_amount;       /* current number of allocated rtskbs */
 extern unsigned int rtskb_amount_max;   /* maximum number of allocated rtskbs */
 
 extern void rtskb_over_panic(struct rtskb *skb, int len, void *here);
 extern void rtskb_under_panic(struct rtskb *skb, int len, void *here);
-extern struct rtskb *new_rtskb(void);
-extern void dispose_rtskb(struct rtskb *skb);
 
-extern struct rtskb *alloc_rtskb(unsigned int size);
-#define dev_alloc_rtskb(len)    alloc_rtskb(len)
+extern struct rtskb *alloc_rtskb(unsigned int size, struct rtskb_head *pool);
+#define dev_alloc_rtskb(len, pool)  alloc_rtskb(len, pool)
 
 extern void kfree_rtskb(struct rtskb *skb);
-#define dev_kfree_rtskb(a)      kfree_rtskb(a)
+#define dev_kfree_rtskb(a)  kfree_rtskb(a)
 
-extern struct rtskb *rtskb_clone(struct rtskb *skb,int gfp_mask);
-extern struct rtskb *rtskb_copy(struct rtskb *skb,int gfp_mask);
-extern struct rtskb *__rtskb_dequeue(struct rtskb_head *list);
-extern struct rtskb *rtskb_dequeue(struct rtskb_head *list);
 
-extern void __rtskb_queue_head(struct rtskb_head *list, struct rtskb *skb);
-extern void rtskb_queue_head(struct rtskb_head *list,struct rtskb *buf);
-extern void __rtskb_queue_tail(struct rtskb_head *list, struct rtskb *skb);
-extern void rtskb_queue_tail(struct rtskb_head *list,struct rtskb *buf);
+/***
+ *  rtsk_queue_head_init - initialize the queue
+ *  @list
+ */
+static inline void rtskb_queue_head_init(struct rtskb_head *list)
+{
+    spin_lock_init(&list->lock);
+    list->first = NULL;
+    list->last  = NULL;
+    list->qlen = 0;
+}
 
-extern int rtskb_queue_len(struct rtskb_head *list);
-extern int rtskb_queue_empty(struct rtskb_head *list);
-extern void rtskb_queue_purge(struct rtskb_head *list);
-extern void rtskb_queue_head_init(struct rtskb_head *list);
+/***
+ *  rtsk_prio_queue_head_init - initialize the prioritized queue
+ *  @list
+ */
+static inline void rtskb_prio_list_init(struct rtskb_prio_list *list,
+                                        int priorities)
+{
+    ASSERT(priorities <= 31, priorities = 31;);
+
+    spin_lock_init(&list->lock);
+    list->usage = 0;
+    memset(list->queue, 0, sizeof(struct rtskb_head) * priorities);
+}
+
+/***
+ *  rtsk_queue_len
+ *  @list
+ */
+static inline int rtskb_queue_len(struct rtskb_head *list)
+{
+    return (list->qlen);
+}
+
+/***
+ *  rtsk_queue_empty
+ *  @list
+ */
+static inline int rtskb_queue_empty(struct rtskb_head *list)
+{
+    return (list->qlen == 0);
+}
+
+/***
+ *  __rtskb_queue_tail - queue a buffer at the list tail (w/o locks)
+ *  @list: list to use
+ *  @skb: buffer to queue
+ */
+static inline void __rtskb_queue_tail(struct rtskb_head *list,
+                                      struct rtskb *skb)
+{
+    skb->head = list;
+    skb->next  = NULL;
+
+    if ( !list->qlen ) {
+        list->first = list->last = skb;
+    } else {
+        list->last->next = skb;
+        list->last = skb;
+    }
+    list->qlen++;
+}
+
+/***
+ *  rtskb_queue_tail - queue a buffer at the list tail (lock protected)
+ *  @list: list to use
+ *  @skb: buffer to queue
+ */
+static inline void rtskb_queue_tail(struct rtskb_head *list, struct rtskb *skb)
+{
+    unsigned long flags;
+    flags = rt_spin_lock_irqsave(&list->lock);
+    __rtskb_queue_tail(list, skb);
+    rt_spin_unlock_irqrestore(flags, &list->lock);
+}
+
+/***
+ *  rtskb_prio_queue_tail - queue a buffer at the prioritized list tail
+ *  @list: list to use
+ *  @skb: buffer to queue
+ */
+static inline void rtskb_prio_queue_tail(struct rtskb_prio_list *list,
+                                         struct rtskb *skb)
+{
+    unsigned long flags;
+
+    ASSERT(skb->priority <= 31, skb->priority = 31;);
+
+    flags = rt_spin_lock_irqsave(&list->lock);
+    __rtskb_queue_tail(&list->queue[skb->priority], skb);
+    __set_bit(skb->priority, &list->usage);
+    rt_spin_unlock_irqrestore(flags, &list->lock);
+}
+
+/***
+ *  __rtskb_dequeue - remove from the head of the queue (w/o locks)
+ *  @list: list to dequeue from
+ */
+static inline struct rtskb *__rtskb_dequeue(struct rtskb_head *list)
+{
+    struct rtskb *result = NULL;
+
+    if (list->qlen > 0) {
+        result=list->first;
+        list->first=result->next;
+        result->next=NULL;
+        list->qlen--;
+    }
+
+    return result;
+}
+
+/***
+ *  rtskb_dequeue - remove from the head of the queue (lock protected)
+ *  @list: list to dequeue from
+ */
+static inline struct rtskb *rtskb_dequeue(struct rtskb_head *list)
+{
+    unsigned long flags;
+    struct rtskb *result;
+
+    flags = rt_spin_lock_irqsave(&list->lock);
+    result = __rtskb_dequeue(list);
+    rt_spin_unlock_irqrestore(flags, &list->lock);
+
+    return result;
+}
+
+/***
+ *  rtskb_prio_dequeue - remove from the head of the prioritized queue
+ *  @list: list to dequeue from
+ */
+static inline struct rtskb *rtskb_prio_dequeue(struct rtskb_prio_list *list)
+{
+    unsigned long flags;
+    int prio;
+    struct rtskb *result;
+    struct rtskb_head *sub_list;
+
+    flags = rt_spin_lock_irqsave(&list->lock);
+    if (list->usage) {
+        prio     = ffz(~list->usage);
+        sub_list = &list->queue[prio];
+        result = __rtskb_dequeue(sub_list);
+        if (rtskb_queue_empty(sub_list))
+            __change_bit(prio, &list->usage);
+    }
+    rt_spin_unlock_irqrestore(flags, &list->lock);
+
+    return result;
+}
+
+/***
+ *  rtskb_queue_purge - clean the queue
+ *  @list
+ */
+static inline void rtskb_queue_purge(struct rtskb_head *list)
+{
+    struct rtskb *skb;
+    while ( (skb=rtskb_dequeue(list))!=NULL )
+        kfree_rtskb(skb);
+}
 
 static inline int rtskb_is_nonlinear(const struct rtskb *skb)
 {
@@ -170,7 +327,8 @@ static inline void rtskb_reserve(struct rtskb *skb, unsigned int len)
     skb->tail+=len;
 }
 
-#define RTSKB_LINEAR_ASSERT(rtskb) do { if (rtskb_is_nonlinear(rtskb)) BUG(); }  while (0)
+#define RTSKB_LINEAR_ASSERT(rtskb) \
+    do { if (rtskb_is_nonlinear(rtskb)) BUG(); }  while (0)
 
 static inline unsigned char *__rtskb_put(struct rtskb *skb, unsigned int len)
 {
@@ -226,7 +384,6 @@ static inline unsigned char *rtskb_pull(struct rtskb *skb, unsigned int len)
     return __rtskb_pull(skb,len);
 }
 
-
 static inline void rtskb_trim(struct rtskb *skb, unsigned int len)
 {
     if (skb->len>len) {
@@ -235,13 +392,38 @@ static inline void rtskb_trim(struct rtskb *skb, unsigned int len)
     }
 }
 
+static inline int rtskb_acquire(struct rtskb *rtskb,
+                                struct rtskb_head *comp_pool)
+{
+    struct rtskb *comp_rtskb = rtskb_dequeue(comp_pool);
 
+    if (!comp_rtskb)
+        return -ENOMEM;
 
-extern struct rtskb_head rtskb_pool;
-extern int rtskb_pool_init(void);
-extern int rtskb_pool_release(void);
+    comp_rtskb->pool = rtskb->pool;
+    rtskb_queue_tail(comp_rtskb->pool, comp_rtskb);
+    rtskb->pool = comp_pool;
 
-extern unsigned int rtskb_copy_and_csum_bits(const struct rtskb *skb, int offset, u8 *to, int len, unsigned int csum);
+    return 0;
+}
+
+extern struct rtskb_head global_pool;
+
+extern unsigned int rtskb_pool_init(struct rtskb_head *pool,
+                                    unsigned int initial_size);
+extern void rtskb_pool_release(struct rtskb_head *pool);
+
+extern unsigned int rtskb_pool_extend(struct rtskb_head *pool,
+                                      unsigned int add_rtskbs);
+extern unsigned int rtskb_pool_shrink(struct rtskb_head *pool,
+                                      unsigned int rem_rtskbs);
+
+extern int rtskb_global_pool_init(void);
+#define rtskb_global_pool_release() rtskb_pool_release(&global_pool)
+
+extern unsigned int rtskb_copy_and_csum_bits(const struct rtskb *skb,
+                                             int offset, u8 *to, int len,
+                                             unsigned int csum);
 extern void rtskb_copy_and_csum_dev(const struct rtskb *skb, u8 *to);
 
 
