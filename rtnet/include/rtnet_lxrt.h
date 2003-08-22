4cc6952204a38cf1ea529ab4bf6cdead925bbfb8
Jan Kiszka <jan.kiszka@web.de>
2003-08-22 16:10:30 +0000

    * added rt_socket_setsockopt * added defines for rt_socket, rt_recv, etc.
    git-svn-id: https://rtnet.svn.sourceforge.net/svnroot/rtnet/trunk@181 be94bdbd-1fe7-0310-aeeb-c95d3c307607

--- rtnet/include/rtnet_lxrt.h
+++ rtnet/include/rtnet_lxrt.h
@@ -1,7 +1,7 @@
 /* rtnet_lxrt.h
  *
  * rtnet_lxrt - real-time networking in usermode
- * 
+ *
  * Copyright (C) 2002 Ulrich Marx <marx@fet.uni-hannover.de>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -23,45 +23,46 @@
 
 #include <rtai_declare.h>
 
-#define LxRTNET_IDX		 7
+#define LxRTNET_IDX             7
 
-#define RT_SOCKET		 0
-#define RT_SOCKET_CLOSE		 1
-#define RT_SOCKET_BIND		 2
-#define RT_SOCKET_CONNECT	 3
-#define RT_SOCKET_ACCEPT	 4
-#define RT_SOCKET_LISTEN	 5
-#define RT_SOCKET_SEND		 6
-#define RT_SOCKET_RECV		 7
-#define RT_SOCKET_SENDTO	 8
-#define RT_SOCKET_RECVFROM	 9
-#define RT_SOCKET_SENDMSG	10
-#define RT_SOCKET_RECVMSG	11
-#define RT_SOCKET_WRITE		12
-#define RT_SOCKET_READ		13
-#define RT_SOCKET_WRITEV	14
-#define RT_SOCKET_READV		15
-#define RT_SOCKET_GETSOCKNAME	16
-#define RT_SOCKET_CALLBACK	17
+#define RT_SOCKET               0
+#define RT_SOCKET_CLOSE         1
+#define RT_SOCKET_BIND          2
+#define RT_SOCKET_CONNECT       3
+#define RT_SOCKET_ACCEPT        4
+#define RT_SOCKET_LISTEN        5
+#define RT_SOCKET_SEND          6
+#define RT_SOCKET_RECV          7
+#define RT_SOCKET_SENDTO        8
+#define RT_SOCKET_RECVFROM      9
+#define RT_SOCKET_SENDMSG       10
+#define RT_SOCKET_RECVMSG       11
+#define RT_SOCKET_WRITE	        12
+#define RT_SOCKET_READ	        13
+#define RT_SOCKET_WRITEV        14
+#define RT_SOCKET_READV         15
+#define RT_SOCKET_GETSOCKNAME   16
+#define RT_SOCKET_SETSOCKOPT    17
 
-#define RT_SSOCKET		18
-#define RT_SSOCKET_CLOSE	19
-#define RT_SSOCKET_BIND		20
-#define RT_SSOCKET_CONNECT	21
-#define RT_SSOCKET_ACCEPT	22
-#define RT_SSOCKET_LISTEN	23
-#define RT_SSOCKET_SEND		24
-#define RT_SSOCKET_RECV		25
-#define RT_SSOCKET_SENDTO	26
-#define RT_SSOCKET_RECVFROM	27
-#define RT_SSOCKET_SENDMSG	28
-#define RT_SSOCKET_RECVMSG	29
-#define RT_SSOCKET_WRITE	30
-#define RT_SSOCKET_READ		31
-#define RT_SSOCKET_WRITEV	32
-#define RT_SSOCKET_READV	33
-#define RT_SSOCKET_GETSOCKNAME	34
-#define RT_SSOCKET_CALLBACK	35
+/* not implemented yet (will they ever be?) */
+#define RT_SSOCKET              40
+#define RT_SSOCKET_CLOSE        41
+#define RT_SSOCKET_BIND         42
+#define RT_SSOCKET_CONNECT      43
+#define RT_SSOCKET_ACCEPT       44
+#define RT_SSOCKET_LISTEN       45
+#define RT_SSOCKET_SEND         46
+#define RT_SSOCKET_RECV         47
+#define RT_SSOCKET_SENDTO       48
+#define RT_SSOCKET_RECVFROM     49
+#define RT_SSOCKET_SENDMSG      50
+#define RT_SSOCKET_RECVMSG      51
+#define RT_SSOCKET_WRITE        52
+#define RT_SSOCKET_READ         53
+#define RT_SSOCKET_WRITEV       54
+#define RT_SSOCKET_READV        55
+#define RT_SSOCKET_GETSOCKNAME  56
+#define RT_SSOCKET_SETSOCKOPT   57
 
 
 extern int rt_socket_init(int family, int type, int protocol);
@@ -70,13 +71,26 @@ extern int rt_socket_bind(int fd, struct sockaddr *addr, int addr_len);
 extern int rt_socket_connect(int fd, struct sockaddr *addr, int addr_len);
 extern int rt_socket_accept(int fd, struct sockaddr *addr, int *addrlen);
 extern int rt_socket_listen(int fd, int backlog);
-extern int rt_socket_send(int fd, void *buf, int len, unsigned int flags); 
+extern int rt_socket_send(int fd, void *buf, int len, unsigned int flags);
 extern int rt_socket_recv(int fd,void *buf,int len, unsigned int flags);
-extern int rt_socket_sendto(int fd, void *buf, int len, unsigned int flags, struct sockaddr *to, int tolen);
-extern int rt_socket_recvfrom(int fd, void *buf, int len, unsigned int flags,  struct sockaddr *from, int *fromlen);
+extern int rt_socket_sendto(int fd, void *buf, int len, unsigned int flags,
+                            struct sockaddr *to, int tolen);
+extern int rt_socket_recvfrom(int fd, void *buf, int len, unsigned int flags,
+                              struct sockaddr *from, int *fromlen);
 extern int rt_socket_sendmsg(int fd, struct msghdr *msg, unsigned int flags);
 extern int rt_socket_recvmsg(int fd, struct msghdr *msg, unsigned int flags);
 extern int rt_socket_getsockname(int fd, struct sockaddr *addr, int addr_len);
+extern int rt_socket_setsockopt(int fd, int level, int optname,
+                                const void *optval, int optlen);
+
+#define rt_bind                 rt_socket_bind
+#define rt_listen               rt_socket_listen
+#define rt_connect              rt_socket_connect
+#define rt_accept               rt_socket_accept
+#define rt_close                rt_socket_close
+#define rt_sendto               rt_socket_sendto
+#define rt_recvfrom             rt_socket_recvfrom
+#define rt_setsockopt           rt_socket_setsockopt
 
 #ifndef __KERNEL__
 
@@ -87,86 +101,92 @@ extern union rtai_lxrt_t rtai_lxrt(short int dynx, short int lsize, int srq, voi
 
 #define SIZARG sizeof(arg)
 
-DECLARE int rt_socket(int family, int type, int protocol) 
+DECLARE int rt_socket(int family, int type, int protocol)
 {
-	struct { int family; int type; int protocol; } arg = { family, type, protocol };
-	return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET, &arg).i[LOW];
+    struct { int family; int type; int protocol; } arg = { family, type, protocol };
+    return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET, &arg).i[LOW];
 }
 
-DECLARE int rt_socket_close(int fd) 
+DECLARE int rt_socket_close(int fd)
 {
-	struct { int fd; } arg = { fd };
-	return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_CLOSE, &arg).i[LOW];
+    struct { int fd; } arg = { fd };
+    return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_CLOSE, &arg).i[LOW];
 }
 
 DECLARE int rt_socket_bind(int fd, struct sockaddr *addr, int addr_len)
 {
-	struct {int fd; struct sockaddr *addr; int addr_len; } arg = { fd, addr, addr_len };
-	return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_BIND, &arg).i[LOW];
+    struct {int fd; struct sockaddr *addr; int addr_len; } arg = { fd, addr, addr_len };
+    return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_BIND, &arg).i[LOW];
 }
 
 DECLARE int rt_socket_connect(int fd, struct sockaddr *addr, int addr_len)
 {
-	struct {int fd; struct sockaddr *addr; int addr_len; } arg = { fd, addr, addr_len };
-	return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_CONNECT, &arg).i[LOW];
+    struct {int fd; struct sockaddr *addr; int addr_len; } arg = { fd, addr, addr_len };
+    return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_CONNECT, &arg).i[LOW];
 }
 
 DECLARE int rt_socket_accept(int fd, struct sockaddr *addr, int *addr_len)
 {
-	struct {int fd; struct sockaddr *addr; int *addr_len; } arg = { fd, addr, addr_len };
-	return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_ACCEPT, &arg).i[LOW];
+    struct {int fd; struct sockaddr *addr; int *addr_len; } arg = { fd, addr, addr_len };
+    return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_ACCEPT, &arg).i[LOW];
 }
 
 DECLARE int rt_socket_listen(int fd, int backlog)
 {
-	struct {int fd; int backlog; } arg = { fd, backlog };
-	return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_LISTEN, &arg).i[LOW];
+    struct {int fd; int backlog; } arg = { fd, backlog };
+    return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_LISTEN, &arg).i[LOW];
 }
 
 DECLARE int rt_socket_send(int fd, void *buf, int len, unsigned int flags)
 {
-	struct {int fd; void *buf; int len; unsigned int flags;} arg = { fd, buf, len, flags };
-	return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_SEND, &arg).i[LOW];
+    struct {int fd; void *buf; int len; unsigned int flags;} arg = { fd, buf, len, flags };
+    return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_SEND, &arg).i[LOW];
 }
 
 DECLARE int rt_socket_recv(int fd, void *buf, int len, unsigned int flags)
 {
-	struct {int fd; void *buf; int len; unsigned int flags;} arg = { fd, buf, len, flags };
-	return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_RECV, &arg).i[LOW];
+    struct {int fd; void *buf; int len; unsigned int flags;} arg = { fd, buf, len, flags };
+    return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_RECV, &arg).i[LOW];
 }
 
 DECLARE int rt_socket_sendto(int fd, void *buf, int len, unsigned int flags, struct sockaddr *to, int tolen)
 {
-	struct {int fd; void *buf; int len; unsigned int flags; struct sockaddr *to; int tolen;} arg 
-		= {fd, buf, len, flags, to, tolen};
-	return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_SENDTO, &arg).i[LOW];	
+    struct {int fd; void *buf; int len; unsigned int flags; struct sockaddr *to; int tolen;} arg
+        = {fd, buf, len, flags, to, tolen};
+    return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_SENDTO, &arg).i[LOW];
 }
 
 DECLARE int rt_socket_recvfrom(int fd, void *buf, int len, unsigned int flags, struct sockaddr *from, int *fromlen)
 {
-	fd = fd; buf=buf; len=len; flags=flags; from=from, fromlen=fromlen;
+    fd = fd; buf=buf; len=len; flags=flags; from=from, fromlen=fromlen;
 #warning function not yet implemented!
-	return 0;
+    return 0;
 }
 
 DECLARE int rt_socket_sendmsg(int fd, struct msghdr *msg, unsigned int flags)
 {
-	struct {int fd; struct msghdr *msg; unsigned int flags;} arg = {fd, msg, flags};
-	return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_SENDMSG, &arg).i[LOW];	
+    struct {int fd; struct msghdr *msg; unsigned int flags;} arg = {fd, msg, flags};
+    return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_SENDMSG, &arg).i[LOW];
 }
 
-DECLARE int rt_socket_recvmsg(int fd, struct msghdr *msg, unsigned int flags) 
+DECLARE int rt_socket_recvmsg(int fd, struct msghdr *msg, unsigned int flags)
 {
-	fd=fd; msg=msg; flags=flags;
+    fd=fd; msg=msg; flags=flags;
 #warning function not yet implemented!
-	return 0;
+    return 0;
 }
 
 DECLARE int rt_socket_getsockname(int fd, struct sockaddr *addr, int addr_len)
 {
-	struct {int fd; struct sockaddr *addr; int addr_len;} arg = {fd, addr, addr_len};
-	return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_GETSOCKNAME, &arg).i[LOW];
-} 
+    struct {int fd; struct sockaddr *addr; int addr_len;} arg = {fd, addr, addr_len};
+    return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_GETSOCKNAME, &arg).i[LOW];
+}
+
+DECLARE int rt_socket_setsockopt(int fd, int level, int optname, const void *optval, int optlen)
+{
+    struct {int fd; int level; int optname; const void *optval; int optlen;} arg = {fd, level, optname, optval, optlen};
+    return (int)rtai_lxrt(LxRTNET_IDX, SIZARG, RT_SOCKET_SETSOCKOPT, &arg).i[LOW];
+}
 
 #endif	/* __KERNEL__ */
 #endif	/* __LX_RTNET_H_ */
--- rtnet/rtnet_lxrt.c
+++ rtnet/rtnet_lxrt.c
@@ -13,7 +13,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
-* You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
@@ -35,53 +35,47 @@
 MODULE_LICENSE("GPL");
 
 static struct rt_fun_entry rt_lxrtnet_fun[] = {
-	[RT_SOCKET]		= {                  0, rt_socket            },
-	[RT_SOCKET_CLOSE]      	= {                  0, rt_socket_close      },
-	[RT_SOCKET_BIND]       	= {	      UR1(2,3), rt_socket_bind       },
-	[RT_SOCKET_CONNECT]	= {           UR1(2,3), rt_socket_connect    },
-	[RT_SOCKET_ACCEPT]	= {           UR1(2,3), rt_socket_accept     },
-        [RT_SOCKET_LISTEN]	= {                  0, rt_socket_listen     },
-        [RT_SOCKET_SEND]       	= {           UR1(2,3), rt_socket_send       },
-	[RT_SOCKET_RECV]       	= {           UW1(2,3), rt_socket_recv       },
-	[RT_SOCKET_SENDTO]	= {UR1(2,3) | UR2(5,6), rt_socket_sendto     },
-	[RT_SOCKET_RECVFROM]	= {UW1(2,3) | UW2(5,6), rt_socket_recvfrom   },
-	[RT_SOCKET_SENDMSG]	= {           UW1(2,3), rt_socket_sendmsg    },
-	[RT_SOCKET_RECVMSG]	= {           UR1(2,3), rt_socket_recvmsg    },
-        [RT_SOCKET_WRITE]      	= {           UW1(2,3), rt_socket_send       },
-        [RT_SOCKET_READ]       	= {           UR1(2,3), rt_socket_recv       },
-	[RT_SOCKET_WRITEV]	= {           UW1(2,3), rt_socket_sendmsg    },
-	[RT_SOCKET_READV]      	= {           UR1(2,3), rt_socket_recvmsg    },
-	[RT_SOCKET_GETSOCKNAME]	= {           UW1(2,3), rt_socket_getsockname},
+    [RT_SOCKET]             = {0,                   rt_socket            },
+    [RT_SOCKET_CLOSE]       = {0,                   rt_socket_close      },
+    [RT_SOCKET_BIND]        = {UR1(2,3),            rt_socket_bind       },
+    [RT_SOCKET_CONNECT]     = {UR1(2,3),            rt_socket_connect    },
+    [RT_SOCKET_ACCEPT]      = {UR1(2,3),            rt_socket_accept     },
+    [RT_SOCKET_LISTEN]      = {0,                   rt_socket_listen     },
+    [RT_SOCKET_SEND]        = {UR1(2,3),            rt_socket_send       },
+    [RT_SOCKET_RECV]        = {UW1(2,3),            rt_socket_recv       },
+    [RT_SOCKET_SENDTO]      = {UR1(2,3) | UR2(5,6), rt_socket_sendto     },
+    [RT_SOCKET_RECVFROM]    = {UW1(2,3) | UW2(5,6), rt_socket_recvfrom   },
+    [RT_SOCKET_SENDMSG]     = {UW1(2,3),            rt_socket_sendmsg    },
+    [RT_SOCKET_RECVMSG]     = {UR1(2,3),            rt_socket_recvmsg    },
+    [RT_SOCKET_WRITE]       = {UW1(2,3),            rt_socket_send       },
+    [RT_SOCKET_READ]        = {UR1(2,3),            rt_socket_recv       },
+    [RT_SOCKET_WRITEV]      = {UW1(2,3),            rt_socket_sendmsg    },
+    [RT_SOCKET_READV]       = {UR1(2,3),            rt_socket_recvmsg    },
+    [RT_SOCKET_GETSOCKNAME] = {UW1(2,3),            rt_socket_getsockname},
+    [RT_SOCKET_SETSOCKOPT]  = {UR1(4,5),            rt_socket_setsockopt }
 };
 
 
 /***
- *	lxrtnet_init
+ *  lxrtnet_init
  */
-static int __init lxrtnet_init(void) 
+static int __init lxrtnet_init(void)
 {
-	if (set_rt_fun_ext_index(rt_lxrtnet_fun, LxRTNET_IDX)) {
-		printk("Recompile your module with a different index\n");
-		return -EACCES;
-	}
+    if (set_rt_fun_ext_index(rt_lxrtnet_fun, LxRTNET_IDX)) {
+        printk("Recompile your module with a different index\n");
+        return -EACCES;
+    }
 
-	return(0);
+    return(0);
 }
 
 /***
- *	lxrtnet_cleanup
+ *  lxrtnet_cleanup
  */
-static void __exit lxrtnet_cleanup(void) 
+static void __exit lxrtnet_cleanup(void)
 {
-	reset_rt_fun_ext_index(rt_lxrtnet_fun, LxRTNET_IDX);
+    reset_rt_fun_ext_index(rt_lxrtnet_fun, LxRTNET_IDX);
 }
 
 module_init(lxrtnet_init);
 module_exit(lxrtnet_cleanup);
-
-
-
-
-
-
-
