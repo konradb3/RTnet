e8697ae191047151d5808176c2beaa553c3c6465
Jan Kiszka <jan.kiszka@web.de>
2003-10-31 16:32:21 +0000

    * initial checkin
    git-svn-id: https://rtnet.svn.sourceforge.net/svnroot/rtnet/trunk@259 be94bdbd-1fe7-0310-aeeb-c95d3c307607

---
+++ rtnet/examples/rtnet_lxrt/rtnet_ifs.c
@@ -0,0 +1,117 @@
+/* rtnet_ifs.c
+ *
+ * Lists all local IP addresses and the interface flags
+ * 10/2003 by Jan Kiszka <jan.kiszka@web.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sched.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <linux/if.h>
+#include <sys/ioctl.h>
+
+#include <rtai_lxrt_user.h>
+#include <rtnet.h>
+
+
+/* Well, we know that there are currently only up to 8... */
+#define MAX_RT_DEVICES  8
+
+
+int main(int argc, char *argv[])
+{
+    int sockfd = 0;
+    struct ifreq ifr[MAX_RT_DEVICES];
+    short flags[MAX_RT_DEVICES];
+    int devices = 0;
+    struct ifconf ifc;
+    struct ifreq flags_ifr;
+    int i, ret;
+
+    RT_TASK *lxrtnettsk;
+
+    printf("RTnet, interface lister for NEWLXRT\n");
+
+    /* Lock allocated memory into RAM. */
+    mlockall(MCL_CURRENT|MCL_FUTURE);
+
+    /* Initialize a real time buddy. */
+    lxrtnettsk = rt_task_init(4800, 1, 0, 0);
+    if (NULL == lxrtnettsk) {
+            printf("CANNOT INIT MASTER TASK\n");
+            exit(1);
+    }
+
+    /* Switch over to hard realtime mode. */
+    rt_make_hard_real_time();
+
+    /* Create new socket. */
+    sockfd = rt_socket(AF_INET, SOCK_DGRAM, 0);
+
+    ifc.ifc_len = sizeof(ifr);
+    ifc.ifc_req = ifr;
+
+    ret = rt_socket_ioctl(sockfd, SIOCGIFCONF, &ifc);
+    if (ret < 0) {
+        rt_socket_close(sockfd);
+        rt_make_soft_real_time();
+        rt_task_delete(lxrtnettsk);
+
+        printf("Error retrieving device list: %d\n", ret);
+        exit(1);
+    }
+
+    while (ifc.ifc_len >= sizeof(struct ifreq)) {
+        memcpy(flags_ifr.ifr_name, ifc.ifc_req[devices].ifr_name, IFNAMSIZ);
+        ret = rt_socket_ioctl(sockfd, SIOCGIFFLAGS, &flags_ifr);
+        if (ret < 0) {
+            rt_socket_close(sockfd);
+            rt_make_soft_real_time();
+            rt_task_delete(lxrtnettsk);
+
+            printf("Error retrieving flags for device %s: %d\n",
+                   flags_ifr.ifr_name, ret);
+            exit(1);
+        }
+        flags[devices] = flags_ifr.ifr_flags;
+        devices++;
+    }
+
+    /* Close socket. */
+    rt_socket_close(sockfd);
+
+    /* Switch over to soft realtime mode. */
+    rt_make_soft_real_time();
+
+    /* Delete realtime buddy. */
+    rt_task_delete(lxrtnettsk);
+
+    for (i = 0; i < devices; i++)
+        printf("Device %s: IP %d.%d.%d.%d, flags %0x08X\n", ifr[i].ifr_name,
+               ((struct sockaddr_in *)&ifr[i].ifr_addr)->sin_addr.s_addr & 0xFF,
+               ((struct sockaddr_in *)&ifr[i].ifr_addr)->sin_addr.s_addr >> 8 & 0xFF,
+               ((struct sockaddr_in *)&ifr[i].ifr_addr)->sin_addr.s_addr >> 16 & 0xFF,
+               ((struct sockaddr_in *)&ifr[i].ifr_addr)->sin_addr.s_addr >> 24,
+               flags[i]);
+
+    return 0;
+}
