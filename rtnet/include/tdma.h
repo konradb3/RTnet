3c4bdfae6e229096b00d295446215c9e85880c93
Jan Kiszka <jan.kiszka@web.de>
2003-11-20 17:37:26 +0000

    * fixed wrong parameter order in rt_spin_unlock_irqrestore
    git-svn-id: https://rtnet.svn.sourceforge.net/svnroot/rtnet/trunk@294 be94bdbd-1fe7-0310-aeeb-c95d3c307607

--- rtnet/include/tdma.h
+++ rtnet/include/tdma.h
@@ -55,7 +55,7 @@ static inline RTIME tdma_get_delta_t(struct rtmac_tdma *tdma)
 
     flags = rt_spin_lock_irqsave(&tdma->delta_t_lock);
     delta_t = tdma->delta_t;
-    rt_spin_unlock_irqrestore(&tdma->delta_t_lock, flags);
+    rt_spin_unlock_irqrestore(flags, &tdma->delta_t_lock);
 
     return delta_t;
 }
