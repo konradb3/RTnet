95eef703fb61818feb9df520a224b7c2b5f45181
Jan Kiszka <jan.kiszka@web.de>
2003-11-28 15:16:45 +0000

    * improved troughput by avoiding an useless copy step
    git-svn-id: https://rtnet.svn.sourceforge.net/svnroot/rtnet/trunk@297 be94bdbd-1fe7-0310-aeeb-c95d3c307607

--- rtnet/driver/loopback-rt.c
+++ rtnet/driver/loopback-rt.c
@@ -29,7 +29,7 @@
 #include <rtnet_internal.h>
 #include <rtnet_port.h>
 
-//#define DEBUG_LOOPBACK_DRIVER
+/*#define DEBUG_LOOPBACK_DRIVER*/
 
 MODULE_AUTHOR("Maintainer: Jan Kiszka <Jan.Kiszka@web.de>");
 MODULE_DESCRIPTION("RTnet loopback driver");
@@ -41,7 +41,7 @@ static struct rtnet_device* rt_loopback_dev;
  *  rt_loopback_open
  *  @rtdev
  */
-static int rt_loopback_open (struct rtnet_device *rtdev) 
+static int rt_loopback_open (struct rtnet_device *rtdev)
 {
     MOD_INC_USE_COUNT;
 
@@ -56,7 +56,7 @@ static int rt_loopback_open (struct rtnet_device *rtdev)
  *  rt_loopback_close
  *  @rtdev
  */
-static int rt_loopback_close (struct rtnet_device *rtdev) 
+static int rt_loopback_close (struct rtnet_device *rtdev)
 {
     rtnetif_stop_queue(rtdev);
     rt_stack_disconnect(rtdev);
@@ -75,67 +75,47 @@ static int rt_loopback_close (struct rtnet_device *rtdev)
  */
 static int rt_loopback_xmit(struct rtskb *skb, struct rtnet_device *rtdev)
 {
-    struct rtskb *new_skb;
+    /* make sure that critical field are re-intialised */
+    skb->chain_end = skb;
 
-    if ( (new_skb=dev_alloc_rtskb(skb->len + 2, &global_pool))==NULL )
-    {
-        rt_printk("RTnet %s: couldn't allocate a rtskb of size %d.\n", rtdev->name, skb->len);
-        kfree_rtskb(skb);
-    }
-    else
-    {
-        new_skb->rx = rt_get_time();
-        new_skb->rtdev = rtdev;
-        rtskb_reserve(new_skb,2);
-        memcpy(new_skb->buf_start, skb->buf_start, SKB_DATA_ALIGN(ETH_FRAME_LEN));
-        rtskb_put(new_skb, skb->len);
-        kfree_rtskb(skb);
-        new_skb->protocol = rt_eth_type_trans(new_skb, rtdev);
+    /* parse the Ethernet header as usual */
+    skb->protocol = rt_eth_type_trans(skb, rtdev);
 
 #ifdef DEBUG_LOOPBACK_DRIVER
-        {
-            int i, cuantos;
-            rt_printk("\n\nPACKET:");
-            rt_printk("\nskb->protocol = %d", skb->protocol);
-            rt_printk("\nskb->pkt_type = %d", skb->pkt_type);
-            rt_printk("\nskb->users = %d", skb->users);
-            rt_printk("\nskb->cloned = %d", skb->cloned);
-            rt_printk("\nskb->csum = %d", skb->csum);
-            rt_printk("\nskb->len = %d", skb->len);
+    {
+        int i, cuantos;
+        rt_printk("\n\nPACKET:");
+        rt_printk("\nskb->protocol = %d", skb->protocol);
+        rt_printk("\nskb->pkt_type = %d", skb->pkt_type);
+        rt_printk("\nskb->csum = %d", skb->csum);
+        rt_printk("\nskb->len = %d", skb->len);
 
-            rt_printk("\nnew_skb->protocol = %d", new_skb->protocol);
-            rt_printk("\nnew_skb->pkt_type = %d", new_skb->pkt_type);
-            rt_printk("\nnew_skb->users = %d", new_skb->users);
-            rt_printk("\nnew_skb->cloned = %d", new_skb->cloned);
-            rt_printk("\nnew_skb->csum = %d", new_skb->csum);
-            rt_printk("\nnew_skb->len = %d", new_skb->len);
+        rt_printk("\n\nETHERNET HEADER:");
+        rt_printk("\nMAC dest: "); for(i=0;i<6;i++){ rt_printk("0x%02X ", skb->buf_start[i+2]); }
+        rt_printk("\nMAC orig: "); for(i=0;i<6;i++){ rt_printk("0x%02X ", skb->buf_start[i+8]); }
+        rt_printk("\nPROTOCOL: "); for(i=0;i<2;i++){ rt_printk("0x%02X ", skb->buf_start[i+14]); }
 
-            rt_printk("\n\nETHERNET HEADER:");
-            rt_printk("\nMAC dest: "); for(i=0;i<6;i++){ rt_printk("0x%02X ", new_skb->buf_start[i+2]); }
-            rt_printk("\nMAC orig: "); for(i=0;i<6;i++){ rt_printk("0x%02X ", new_skb->buf_start[i+8]); }
-            rt_printk("\nPROTOCOL: "); for(i=0;i<2;i++){ rt_printk("0x%02X ", new_skb->buf_start[i+14]); }
+        rt_printk("\n\nIP HEADER:");
+        rt_printk("\nVERSIZE : "); for(i=0;i<1;i++){ rt_printk("0x%02X ", skb->buf_start[i+16]); }
+        rt_printk("\nPRIORITY: "); for(i=0;i<1;i++){ rt_printk("0x%02X ", skb->buf_start[i+17]); }
+        rt_printk("\nLENGTH  : "); for(i=0;i<2;i++){ rt_printk("0x%02X ", skb->buf_start[i+18]); }
+        rt_printk("\nIDENT   : "); for(i=0;i<2;i++){ rt_printk("0x%02X ", skb->buf_start[i+20]); }
+        rt_printk("\nFRAGMENT: "); for(i=0;i<2;i++){ rt_printk("0x%02X ", skb->buf_start[i+22]); }
+        rt_printk("\nTTL     : "); for(i=0;i<1;i++){ rt_printk("0x%02X ", skb->buf_start[i+24]); }
+        rt_printk("\nPROTOCOL: "); for(i=0;i<1;i++){ rt_printk("0x%02X ", skb->buf_start[i+25]); }
+        rt_printk("\nCHECKSUM: "); for(i=0;i<2;i++){ rt_printk("0x%02X ", skb->buf_start[i+26]); }
+        rt_printk("\nIP ORIGE: "); for(i=0;i<4;i++){ rt_printk("0x%02X ", skb->buf_start[i+28]); }
+        rt_printk("\nIP DESTI: "); for(i=0;i<4;i++){ rt_printk("0x%02X ", skb->buf_start[i+32]); }
 
-            rt_printk("\n\nIP HEADER:");
-            rt_printk("\nVERSIZE : "); for(i=0;i<1;i++){ rt_printk("0x%02X ", new_skb->buf_start[i+16]); }
-            rt_printk("\nPRIORITY: "); for(i=0;i<1;i++){ rt_printk("0x%02X ", new_skb->buf_start[i+17]); }
-            rt_printk("\nLENGTH  : "); for(i=0;i<2;i++){ rt_printk("0x%02X ", new_skb->buf_start[i+18]); }
-            rt_printk("\nIDENT   : "); for(i=0;i<2;i++){ rt_printk("0x%02X ", new_skb->buf_start[i+20]); }
-            rt_printk("\nFRAGMENT: "); for(i=0;i<2;i++){ rt_printk("0x%02X ", new_skb->buf_start[i+22]); }
-            rt_printk("\nTTL     : "); for(i=0;i<1;i++){ rt_printk("0x%02X ", new_skb->buf_start[i+24]); }
-            rt_printk("\nPROTOCOL: "); for(i=0;i<1;i++){ rt_printk("0x%02X ", new_skb->buf_start[i+25]); }
-            rt_printk("\nCHECKSUM: "); for(i=0;i<2;i++){ rt_printk("0x%02X ", new_skb->buf_start[i+26]); }
-            rt_printk("\nIP ORIGE: "); for(i=0;i<4;i++){ rt_printk("0x%02X ", new_skb->buf_start[i+28]); }
-            rt_printk("\nIP DESTI: "); for(i=0;i<4;i++){ rt_printk("0x%02X ", new_skb->buf_start[i+32]); }
-
-            cuantos = (int)(*(unsigned short *)(new_skb->buf_start+18)) - 20;
-            rt_printk("\n\nDATA (%d):", cuantos);
-            rt_printk("\n:"); for(i=0;i<cuantos;i++){ rt_printk("0x%02X ", new_skb->buf_start[i+36]); }
-        }
+        cuantos = (int)(*(unsigned short *)(skb->buf_start+18)) - 20;
+        rt_printk("\n\nDATA (%d):", cuantos);
+        rt_printk("\n:"); for(i=0;i<cuantos;i++){ rt_printk("0x%02X ", skb->buf_start[i+36]); }
+    }
 #endif
 
-        rtnetif_rx(new_skb);
-        rt_mark_stack_mgr(rtdev);
-    }
+    rtnetif_rx(skb);
+    rt_mark_stack_mgr(rtdev);
+
     return 0;
 }
 
@@ -150,8 +130,8 @@ static int __init loopback_init(void)
 
     rt_printk("initializing loopback...\n");
 
-    if ( (rtdev=rt_alloc_etherdev(0))==NULL )
-            return -ENODEV;
+    if ((rtdev = rt_alloc_etherdev(0)) == NULL)
+        return -ENODEV;
 
     rt_rtdev_connect(rtdev, &RTDEV_manager);
     SET_MODULE_OWNER(rtdev);
@@ -163,10 +143,10 @@ static int __init loopback_init(void)
     rtdev->hard_header = rt_eth_header;
     rtdev->hard_start_xmit = &rt_loopback_xmit;
 
-    if ( (err = rt_register_rtnetdev(rtdev)) )
-        {
+    if ((err = rt_register_rtnetdev(rtdev)) != 0)
+    {
         rtdev_free(rtdev);
-                return err;
+        return err;
     }
 
     rt_loopback_dev = rtdev;
