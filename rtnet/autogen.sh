4e7b7fdba0a2ee666380ebf9d7510d8a52474ecc
Jan Kiszka <jan.kiszka@web.de>
2003-12-18 14:24:56 +0000

    * "make install" now also installs modules * fixed deletion of files in config/autoconf by autogen.sh [patch by Marc]
    git-svn-id: https://rtnet.svn.sourceforge.net/svnroot/rtnet/trunk@309 be94bdbd-1fe7-0310-aeeb-c95d3c307607

--- rtnet/autogen.sh
+++ rtnet/autogen.sh
@@ -5,7 +5,7 @@ rm -f configure config.log config.guess
 rm -f config.status aclocal.m4
 rm -f `find . -name 'Makefile.in'`
 rm -f `find . -name 'Makefile'`
-rm -rf config/autoconf
+rm -f config/autoconf/*
 mkdir -p config/autoconf
 
 aclocal
