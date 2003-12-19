b0b851ad684e7758b9ab37f3db0c4475b986f9d4
Jan Kiszka <jan.kiszka@web.de>
2003-12-19 13:17:26 +0000

    * added rtnetif_carrier_ok for natsemi-rt
    git-svn-id: https://rtnet.svn.sourceforge.net/svnroot/rtnet/trunk@310 be94bdbd-1fe7-0310-aeeb-c95d3c307607

--- rtnet/include/rtnet_port.h
+++ rtnet/include/rtnet_port.h
@@ -35,42 +35,47 @@
 
 static inline void rtnetif_start_queue(struct rtnet_device *rtdev)
 {
-	clear_bit(__LINK_STATE_XOFF, &rtdev->state);
+    clear_bit(__LINK_STATE_XOFF, &rtdev->state);
 }
 
 static inline void rtnetif_wake_queue(struct rtnet_device *rtdev)
 {
     if (test_and_clear_bit(__LINK_STATE_XOFF, &rtdev->state))
-	/*TODO __netif_schedule(dev); */ ;
+    /*TODO __netif_schedule(dev); */ ;
 }
 
 static inline void rtnetif_stop_queue(struct rtnet_device *rtdev)
 {
-	set_bit(__LINK_STATE_XOFF, &rtdev->state);
+    set_bit(__LINK_STATE_XOFF, &rtdev->state);
 }
 
 static inline int rtnetif_queue_stopped(struct rtnet_device *rtdev)
 {
-	return test_bit(__LINK_STATE_XOFF, &rtdev->state);
+    return test_bit(__LINK_STATE_XOFF, &rtdev->state);
 }
 
 static inline int rtnetif_running(struct rtnet_device *rtdev)
 {
-	return test_bit(__LINK_STATE_START, &rtdev->state);
+    return test_bit(__LINK_STATE_START, &rtdev->state);
 }
 
 static inline void rtnetif_carrier_on(struct rtnet_device *rtdev)
 {
-	clear_bit(__LINK_STATE_NOCARRIER, &rtdev->state);
-	/*
-	if (netif_running(dev))
-		__netdev_watchdog_up(dev);
-	*/
+    clear_bit(__LINK_STATE_NOCARRIER, &rtdev->state);
+    /*
+    if (netif_running(dev))
+        __netdev_watchdog_up(dev);
+    */
 }
 
 static inline void rtnetif_carrier_off(struct rtnet_device *rtdev)
 {
-	set_bit(__LINK_STATE_NOCARRIER, &rtdev->state);
+    set_bit(__LINK_STATE_NOCARRIER, &rtdev->state);
+}
+
+static inline int rtnetif_carrier_ok(struct rtnet_device *rtdev)
+{
+    return !test_bit(__LINK_STATE_NOCARRIER, &rtdev->state);
 }
 
 
