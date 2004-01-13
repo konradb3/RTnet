72af0300ce3e28fdfeac86f26c7b92d5a343baed
Marc Kleine-Budde <kleine-budde@gmx.de>
2004-01-13 13:53:37 +0000

    * improved cleanup
    git-svn-id: https://rtnet.svn.sourceforge.net/svnroot/rtnet/trunk@360 be94bdbd-1fe7-0310-aeeb-c95d3c307607

--- rtnet/autogen.sh
+++ rtnet/autogen.sh
@@ -1,12 +1,33 @@
 #!/bin/sh
 
 # Clean up the generated crud
-rm -f configure config.log config.guess
-rm -f config.status aclocal.m4
-rm -f `find . -name 'Makefile.in'`
-rm -f `find . -name 'Makefile'`
-rm -f config/autoconf/*
-mkdir -p config/autoconf
+(
+    cd config/autoconf
+    for FILE in compile config.guess config.sub depcomp install-sh ltmain.sh missing mkinstalldirs; do
+	if test -f $FILE; then
+	    rm -f $FILE
+	fi
+    done
+)
+
+for FILE in aclocal.m4 configure config/config.h.in; do
+    if test -f $FILE; then
+	rm -f $FILE
+    fi
+done
+
+for DIR in autom4te.cache; do
+    if test -d $DIR; then
+	rm -rf $DIR
+    fi
+done
+
+find . -type f -name 'Makefile.in' -print0 | xargs -0 rm -f
+find . -type f -name 'Makefile' -print0 | xargs -0 rm -f
+
+if test "x${1}" = "xclean"; then
+    exit
+fi
 
 aclocal
 libtoolize --force --copy
