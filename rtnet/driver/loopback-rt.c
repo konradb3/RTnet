501528ea87bced7eabd68d62550393e575acba7a
Jan Kiszka <jan.kiszka@web.de>
2004-03-11 16:39:56 +0000

    * by-pass stack manager in rt_loopback_xmit * code clean-up (debugging no longer needed => RTcap)
    git-svn-id: https://rtnet.svn.sourceforge.net/svnroot/rtnet/trunk@472 be94bdbd-1fe7-0310-aeeb-c95d3c307607

--- rtnet/driver/loopback-rt.c
+++ rtnet/driver/loopback-rt.c
@@ -26,9 +26,6 @@
 
 #include <rtnet_port.h>
 
-/*#define DEBUG_LOOPBACK*/
-/*#define DEBUG_LOOPBACK_DATA*/
-
 MODULE_AUTHOR("Maintainer: Jan Kiszka <Jan.Kiszka@web.de>");
 MODULE_DESCRIPTION("RTnet loopback driver");
 MODULE_LICENSE("GPL");
@@ -73,48 +70,46 @@ static int rt_loopback_close (struct rtnet_device *rtdev)
  */
 static int rt_loopback_xmit(struct rtskb *skb, struct rtnet_device *rtdev)
 {
+    unsigned short          hash;
+    struct rtpacket_type    *pt;
+    unsigned long           flags;
+
+
     /* make sure that critical fields are re-intialised */
     skb->chain_end = skb;
 
     /* parse the Ethernet header as usual */
     skb->protocol = rt_eth_type_trans(skb, rtdev);
+    skb->nh.raw   = skb->data;
 
-#ifdef DEBUG_LOOPBACK
-    {
-        int i, cuantos;
-        rtos_print("\n\nPACKET:");
-        rtos_print("\nskb->protocol = %d", skb->protocol);
-        rtos_print("\nskb->pkt_type = %d", skb->pkt_type);
-        rtos_print("\nskb->csum = %d", skb->csum);
-        rtos_print("\nskb->len = %d", skb->len);
+    rtdev_reference(rtdev);
 
-        rtos_print("\n\nETHERNET HEADER:");
-        rtos_print("\nMAC dest: "); for(i=0;i<6;i++){ rtos_print("0x%02X ", skb->buf_start[i+2]); }
-        rtos_print("\nMAC orig: "); for(i=0;i<6;i++){ rtos_print("0x%02X ", skb->buf_start[i+8]); }
-        rtos_print("\nPROTOCOL: "); for(i=0;i<2;i++){ rtos_print("0x%02X ", skb->buf_start[i+14]); }
+    rtcap_report_incoming(skb);
 
-        rtos_print("\n\nIP HEADER:");
-        rtos_print("\nVERSIZE : "); for(i=0;i<1;i++){ rtos_print("0x%02X ", skb->buf_start[i+16]); }
-        rtos_print("\nPRIORITY: "); for(i=0;i<1;i++){ rtos_print("0x%02X ", skb->buf_start[i+17]); }
-        rtos_print("\nLENGTH  : "); for(i=0;i<2;i++){ rtos_print("0x%02X ", skb->buf_start[i+18]); }
-        rtos_print("\nIDENT   : "); for(i=0;i<2;i++){ rtos_print("0x%02X ", skb->buf_start[i+20]); }
-        rtos_print("\nFRAGMENT: "); for(i=0;i<2;i++){ rtos_print("0x%02X ", skb->buf_start[i+22]); }
-        rtos_print("\nTTL     : "); for(i=0;i<1;i++){ rtos_print("0x%02X ", skb->buf_start[i+24]); }
-        rtos_print("\nPROTOCOL: "); for(i=0;i<1;i++){ rtos_print("0x%02X ", skb->buf_start[i+25]); }
-        rtos_print("\nCHECKSUM: "); for(i=0;i<2;i++){ rtos_print("0x%02X ", skb->buf_start[i+26]); }
-        rtos_print("\nIP ORIGE: "); for(i=0;i<4;i++){ rtos_print("0x%02X ", skb->buf_start[i+28]); }
-        rtos_print("\nIP DESTI: "); for(i=0;i<4;i++){ rtos_print("0x%02X ", skb->buf_start[i+32]); }
+    hash = ntohs(skb->protocol) & (MAX_RT_PROTOCOLS-1);
 
-#ifdef DEBUG_LOOPBACK_DATA
-        cuantos = (int)(*(unsigned short *)(skb->buf_start+18)) - 20;
-        rtos_print("\n\nDATA (%d):", cuantos);
-        rtos_print("\n:"); for(i=0;i<cuantos;i++){ rtos_print("0x%02X ", skb->buf_start[i+36]); }
-#endif /* DEBUG_LOOPBACK_DATA */
+    rtos_spin_lock_irqsave(&rt_packets_lock, flags);
+
+    pt = rt_packets[hash];
+
+    if ((pt != NULL) && (pt->type == skb->protocol)) {
+        pt->refcount++;
+        rtos_spin_unlock_irqrestore(&rt_packets_lock, flags);
+
+        pt->handler(skb, pt);
+
+        rtos_spin_lock_irqsave(&rt_packets_lock, flags);
+        pt->refcount--;
+        rtos_spin_unlock_irqrestore(&rt_packets_lock, flags);
+    } else {
+        rtos_spin_unlock_irqrestore(&rt_packets_lock, flags);
+
+        rtos_print("RTnet: unknown layer-3 protocol\n");
+
+        kfree_rtskb(skb);
     }
-#endif /* DEBUG_LOOPBACK */
 
-    rtnetif_rx(skb);
-    rt_mark_stack_mgr(rtdev);
+    rtdev_dereference(rtdev);
 
     return 0;
 }
