689a0c73a493e584d90d8b62e4af4cf7740c6942
Jan Kiszka <jan.kiszka@web.de>
2007-10-17 19:38:19 +0000

    Refactoring
    git-svn-id: https://rtnet.svn.sourceforge.net/svnroot/rtnet/trunk@1139 be94bdbd-1fe7-0310-aeeb-c95d3c307607

--- rtnet/examples/generic/linux_server.c
+++ rtnet/examples/generic/linux_server.c
@@ -14,56 +14,54 @@ char mesg[MAX_MESG_SIZE];
 
 int main(int argc, char *argv[])
 {
-    int                 udpSocket;
-    int                 port;
-    int                 status;
-    int                 size;
-    socklen_t           clientLength;
-    struct sockaddr_in  serverName;
-    struct sockaddr_in  clientName;
+	int                 udpSocket;
+	int                 port;
+	int                 status;
+	int                 size;
+	socklen_t           clientLength;
+	struct sockaddr_in  serverName;
+	struct sockaddr_in  clientName;
 
 
-    if (argc!=2) {
-        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-        exit(1);
-    }
+	if (argc!=2) {
+		fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+		exit(1);
+	}
 
-    port  = atoi(argv[1]);
+	port = atoi(argv[1]);
 
-    udpSocket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (udpSocket<0) {
-        perror("socket()");
-        exit(1);
-    }
+	udpSocket = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (udpSocket<0) {
+		perror("socket()");
+		exit(1);
+	}
 
-    memset(&serverName, 0, sizeof(serverName));
-    serverName.sin_family = AF_INET;
-    serverName.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverName.sin_port = htons(port);
+	memset(&serverName, 0, sizeof(serverName));
+	serverName.sin_family = AF_INET;
+	serverName.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverName.sin_port = htons(port);
 
-    status = bind(udpSocket, (struct sockaddr *)&serverName,
-                  sizeof(serverName));
-    if (status<0) {
-        perror("bind()");
-        exit(1);
-    }
+	status = bind(udpSocket, (struct sockaddr *)&serverName,
+		      sizeof(serverName));
+	if (status<0) {
+		perror("bind()");
+		exit(1);
+	}
 
-    for (;;) {
-        clientLength=sizeof(clientName);
-        size = recvfrom(udpSocket, mesg, MAX_MESG_SIZE, 0,
-                        (struct sockaddr *)&clientName, &clientLength);
-        if (size == -1) {
-            perror("recvfrom()");
-            exit(1);
-        }
-        printf("RCV: %s from %s\n", mesg, inet_ntoa(clientName.sin_addr));
+	for (;;) {
+		clientLength = sizeof(clientName);
+		size = recvfrom(udpSocket, mesg, MAX_MESG_SIZE, 0,
+				(struct sockaddr *)&clientName, &clientLength);
+		if (size == -1) {
+			perror("recvfrom()");
+			exit(1);
+		}
+		printf("RCV: %s from %s\n", mesg,
+		       inet_ntoa(clientName.sin_addr));
 
-        sendto(udpSocket, mesg, size, 0, (struct sockaddr *)&clientName,
-               clientLength);
-    }
+		sendto(udpSocket, mesg, size, 0,
+		       (struct sockaddr *)&clientName, clientLength);
+	}
 
-    return 0;
+	return 0;
 }
-
-
-
